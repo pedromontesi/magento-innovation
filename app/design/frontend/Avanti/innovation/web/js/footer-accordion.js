33aa70774096d357c911141f4bc5c1b42b807fc2
pedromontesi <joaopedromonteiro090@hotmail.com>
2025-12-03 18:59:54 -0300

    footer mobile mostly maked

---
+++ app/design/frontend/Avanti/innovation/web/js/footer-accordion.js
@@ -0,0 +1,24 @@
+const firstAccordion = $('#accordion-1');
+// const firstFooterContent = $('');
+
+firstAccordion.on('click', function () {
+   // firstFooterContent.toggleClass('is-open');
+});
+
+
+
+const secondAccordion = $('#accordion-2');
+// const secondFooterContent = $('');
+
+secondAccordion.on('click', function () {
+   //  secondAccordion.toggleClass('is-open');
+});
+
+
+
+const thirdAccordion = $('#accordion-3');
+// const thirdFooterContent = $('');
+
+thirdAccordion.on('click', function () {
+    //  thirdAccordion.toggleClass('is-open');
+});
--- app/design/frontend/Avanti/innovation/web/js/mobile-search.js
+++
@@ -1,7 +0,0 @@
-define(['jquery'], function ($) {
-    'use strict';
-
-    return function () {
-        $('.label[data-role="minisearch-label"]').addClass('active');
-    };
-});
