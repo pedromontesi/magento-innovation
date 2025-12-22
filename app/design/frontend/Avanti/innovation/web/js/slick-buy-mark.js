52c6d88be70f7462c9e394204b6f5f06c86c3b04
pedromontesi <joaopedromonteiro090@hotmail.com>
2025-12-22 17:33:26 -0300

    more adjustments

--- app/design/frontend/Avanti/innovation/web/js/slick-buy-mark.js
+++ app/design/frontend/Avanti/innovation/web/js/slick-buy-mark.js
@@ -5,20 +5,35 @@ define([
 ], function ($, slick, mediaCheck) {
     'use strict';
 
+    // SLIDER DE BANNER
+    if ($('.slick-banner').length &&
+        !$('.slick-banner').hasClass('slick-initialized')) {
+
+        $('.slick-banner').slick({
+            arrows: true,
+
+        });
+    }
+
+    //SLIDER DE PRODUTOS
     mediaCheck({
         media: '(min-width: 769px)',
-        entry: function () {
 
-            if (!$('.slick-buy-mark').hasClass('slick-initialized')) {
+        entry: function () {
+            if ($('.slick-buy-mark').length &&
+                !$('.slick-buy-mark').hasClass('slick-initialized')) {
+
                 $('.slick-buy-mark').slick({
                     dots: false,
                     infinite: true,
                     speed: 300,
                     slidesToShow: 6,
-                    slidesToScroll: 6
+                    slidesToScroll: 6,
+                    arrows: true
                 });
             }
         },
+
         exit: function () {
             if ($('.slick-buy-mark').hasClass('slick-initialized')) {
                 $('.slick-buy-mark').slick('unslick');
